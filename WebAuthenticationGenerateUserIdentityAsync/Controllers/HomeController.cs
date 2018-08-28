621149ef2b1ff36aeaa4c42e5246c361fea4b7bf
Anbarasu Angappan <anburvhs@hotmail.com>
2018-08-28 10:33:19 +0530

    Add project files.

---
+++ WebAuthenticationGenerateUserIdentityAsync/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAuthenticationGenerateUserIdentityAsync.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var x = User.Identity.Name;
+            var identity = (ClaimsIdentity)User.Identity;
+
+
+            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+            Claim claim = claimsIdentity?.FindFirst(ClaimTypes.Name);
+
+            string userclaims = identity.Name.ToString();
+            if (((System.Security.Claims.ClaimsIdentity)User.Identity).HasClaim(ClaimTypes.Name, claim.Value))
+            {
+                ViewBag.Message = "Your application description page.";
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAuthenticationGenerateUserIdentityAsync/Models/IdentityModels.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebAuthenticationGenerateUserIdentityAsync.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            //ClaimsIdentity claimsIdentity = userIdentity as ClaimsIdentity;
+            //Claim claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+
+            userIdentity.AddClaim(new Claim(ClaimTypes.Name, UserName));
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    //public string GetName(this IIdentity identity)
+    //{
+    //    ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+    //    Claim claim = claimsIdentity?.FindFirst(ClaimTypes.Name);
+
+    //    return claim?.Value ?? string.Empty;
+    //}
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
